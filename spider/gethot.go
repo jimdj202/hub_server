0a77cbc2aa1ba3d7d6d4f4181cc428d55ef3ae2e
Lei <516292026@qq.com>
2020-09-03 22:22:16 +0800

    添加 网易新闻 水木社区

--- spider/gethot.go
+++ spider/gethot.go
@@ -51,7 +51,7 @@ func main(){
 			SpiderNames = append(SpiderNames,fileName)
 		}
 	}
-	SpiderNames = []string{"HacPai"}
+	SpiderNames = []string{"WaterWood"}
 	for _,funcName := range SpiderNames {
 		reflectValue := reflect.ValueOf(&spiders.Spider{Name: funcName})
 		//reflectValueEle := reflectValue.Elem()
